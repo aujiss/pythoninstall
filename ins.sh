415e20f2ca8bd60ccb06e201c5caea6813a1efb6
aujiss <leedgkhoaa@gmail.com>
2024-11-03 16:58:35 +0700

    init project

--- ins.sh
+++ ins.sh
@@ -7,10 +7,20 @@ pkg uninstall python3.11 -y
 pkg install python3.11 -y
 mv -f /data/data/com.termux/files/usr/bin/python3.11 /data/data/com.termux/files/usr/bin/python
 
-echo "Successfully installed python 3.11 !"
+echo "\n\nSuccessfully installed python 3.11 !"
 
 sleep 3 
 
-echo "pip package installation in progress"
+echo "\n\npip package installation in progress"
 
+sleep 3 
+
+wget https://raw.githubusercontent.com/aujiss/pythoninstall/refs/heads/main/ins-pip.py
 python ins-pip.py
+rm ins-pip.py 
+
+echo "\n\nSuccessfully installation pip package"
+
+sleep 3 
+
+exit
