73a1f0b2431d28219a2ed0387c141df4c9ecc219
aujiss <leedgkhoaa@gmail.com>
2024-11-03 17:50:33 +0700

    update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,26 +1,23 @@
 clear
 pkg update
 yes | pkg upgrade -y
-pkg i tur-repo -y
+pkg in wget -y
+pkg in tur-repo -y
 pkg uninstall python -y
 pkg uninstall python3.11 -y
 pkg install python3.11 -y
 mv -f /data/data/com.termux/files/usr/bin/python3.11 /data/data/com.termux/files/usr/bin/python
-
-echo "\n\nSuccessfully installed python 3.11 !"
-
-sleep 3 
-
-echo "\n\npip package installation in progress"
-
-sleep 3 
-
 wget https://raw.githubusercontent.com/aujiss/pythoninstall/refs/heads/main/ins-pip.py
 python ins-pip.py
 rm ins-pip.py 
 
-echo "\n\nSuccessfully installation pip package"
+echo ""
+echo ""
+echo "Successfully installation python 3.11"
 
-sleep 3 
+sleep 3
+
+python --version
+pip --version
 
 exit
